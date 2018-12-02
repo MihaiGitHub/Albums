b079157412223bca724dc65c73bdc4808f3484df
Michael S <mihai.sanfran@gmail.com>
2018-12-01 17:38:17 -0700

    Install and import axios library

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component {
+    // Anytime that the component is about to render this lifecycle method will be called
+    componentWillMount() {
+        debugger;
+        console.log('componentWillMount in AlbumList1111')
+    }
+
     render() {
         return (
             <View>
