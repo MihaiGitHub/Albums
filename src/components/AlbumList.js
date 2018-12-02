976c403464a6f85bc515e1028a3c99b45a9866dd
Michael S <mihai.sanfran@gmail.com>
2018-12-01 17:54:02 -0700

    Create AJAX call to GET albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,8 +5,9 @@ import axios from 'axios';
 class AlbumList extends Component {
     // Anytime that the component is about to render this lifecycle method will be called
     componentWillMount() {
-        debugger;
-        console.log('componentWillMount in AlbumList1111')
+        // Returns a promise that .then will be called once the http call is complete
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => console.log(response))
     }
 
     render() {
