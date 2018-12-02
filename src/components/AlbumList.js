8c30e016b603fc9e915d30f84942d1e29dcf6b7d
Michael S <mihai.sanfran@gmail.com>
2018-12-01 21:54:51 -0700

    Initialize and update state with album list

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,14 +3,19 @@ import { View, Text } from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+    // Initialize state
+    state = { albums: [] };
+
     // Anytime that the component is about to render this lifecycle method will be called
     componentWillMount() {
         // Returns a promise that .then will be called once the http call is complete
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-            .then(response => console.log(response))
+            .then(response => this.setState({ albums: response.data })); // Update state
     }
 
     render() {
+        console.log(this.state);
+        
         return (
             <View>
                 <Text>Album List!!!!</Text>
